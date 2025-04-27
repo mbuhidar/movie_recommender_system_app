d0dd5c0fe555f9b0fcad50eb051b8b962aef05a0
Michael Buhidar <mbuhidar@gmail.com>
2025-04-27 06:35:30 -0400

    More tweaks to movie masking.

--- movie_rec_streamlit_prod.py
+++ movie_rec_streamlit_prod.py
@@ -303,10 +303,10 @@ if st.session_state.genre_selected:
         # Create boolean mask of movies NOT in rated_movie_ids
         mask = ~df_recommendations['movieId'].isin(rated_movie_ids)
         # print("Mask:", mask)
-        # print("Filtered Recommendations:", df_recommendations[mask])
+        print("Filtered Recommendations:", df_recommendations[mask])
         # mask = ~df_recommendations['movieId'].isin(df_new_user_ratings['movieId'].values)
         # Apply mask to keep only unrated movies
-        df_recommendations[mask]
+        df_recommendations = df_recommendations[mask]
         df_recommendations = df_recommendations.sort_values(by='predicted_rating', ascending=False)
 
         # Convert predicted ratings to 1 decimal place
